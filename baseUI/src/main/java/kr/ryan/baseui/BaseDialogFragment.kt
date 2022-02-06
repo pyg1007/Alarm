23985f8af6f1b317842e4b40de347830cddd0bf6
Yong <pyg941007@hanmail.net>
2022-02-06 18:55:25 +0900

    change path

--- baseUI/src/main/java/kr/ryan/baseui/BaseDialogFragment.kt
+++ baseUI/src/main/java/kr/ryan/baseui/BaseDialogFragment.kt
@@ -1,19 +1,49 @@
 package kr.ryan.baseui
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.DialogFragment
+import timber.log.Timber
 
-abstract class BaseDialogFragment<VDB: ViewDataBinding>(@LayoutRes private val layoutRes: Int) : DialogFragment(){
+/**
+ * WeatherAlarm
+ * Class: BaseDialogFragment
+ * Created by pyg10.
+ * Created On 2021-11-18.
+ * Description:
+ */
+abstract class BaseDialogFragment<VDB : ViewDataBinding>(@LayoutRes private val layoutRes: Int) :
+    DialogFragment() {
 
     private var _binding: VDB? = null
     protected val binding: VDB
         get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initDialog()
+        //isCancelable = false
+    }
+
+    private fun initDialog() {
+        val layoutParams = WindowManager.LayoutParams().apply {
+            flags = WindowManager.LayoutParams.FLAG_DIM_BEHIND // 뒤의 배경이 흐려지게 표시
+            dimAmount = 0.8f
+        }
+
+        dialog?.window?.apply {
+            attributes = layoutParams
+            setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT)) // 투명하게
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,11 +51,13 @@ abstract class BaseDialogFragment<VDB: ViewDataBinding>(@LayoutRes private val l
         savedInstanceState: Bundle?
     ): View? {
         _binding = DataBindingUtil.inflate(inflater, layoutRes, container, false)
+        Timber.d("onCreateView $_binding")
         return binding.root
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
+    override fun onDestroyView() {
+        super.onDestroyView()
         _binding = null
+        Timber.d("onDestroyView $_binding")
     }
 }
