86b92ee7a6f75b9c7c2c09bca0301327c85831ea
Yong <pyg941007@hanmail.net>
2022-02-19 23:57:08 +0900

    [add] location 설정이 꺼져있다면 가져올수없기때문에 꺼져있다면 다시 킬수있는 로직을 구성해야함! - activity result launcher 구성완료

--- app/src/main/java/kr/ryan/weatheralarm/future/activity/SplashActivity.kt
+++ app/src/main/java/kr/ryan/weatheralarm/future/activity/SplashActivity.kt
@@ -3,9 +3,15 @@ package kr.ryan.weatheralarm.future.activity
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.whenCreated
+import androidx.lifecycle.whenStarted
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -29,6 +35,28 @@ class SplashActivity : AppCompatActivity() {
         android.Manifest.permission.ACCESS_FINE_LOCATION
     )
 
+    private lateinit var settingsLauncher: ActivityResultLauncher<Intent>
+
+    init {
+
+        lifecycleScope.launch {
+
+            whenStarted {
+
+            }
+
+            whenCreated {
+                initLocationLauncher()
+
+                CoroutineScope(Dispatchers.Main).launch {
+
+                }
+            }
+
+        }
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         installSplashScreen()
@@ -37,27 +65,43 @@ class SplashActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_splash)
 
-        CoroutineScope(Dispatchers.Main).launch {
-            weatherViewModel.weather.collect {
-                if (it == null) {
-                    requestPermission({
-                        this@SplashActivity.getCurrentLatXLngY()?.let { latXLngY ->
-                            Timber.d("location x = ${latXLngY.x} y = ${latXLngY.y} lat = ${latXLngY.lat} lon = ${latXLngY.lng}")
-                            routeNextActivity()
-                        } ?: run {
-                            Timber.d("location Error")
-                            routeNextActivity()
-                        }
-                    },{
-                        Timber.d("denied")
-                    }, *permissions)
-                }else{
-                    Timber.d("not null $it")
-                    routeNextActivity()
+    }
+
+    private fun initLocationLauncher(){
+        settingsLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
+            if (result.resultCode == RESULT_OK){
+                CoroutineScope(Dispatchers.Main).launch {
+                    observeWeather()
                 }
             }
         }
+    }
 
+    private fun routeSettingsLocation(){
+        Intent(Settings.ACTION_LOCALE_SETTINGS).also {
+            settingsLauncher.launch(it)
+        }
+    }
+
+    private suspend fun observeWeather(){
+        weatherViewModel.weather.collect {
+            if (it == null) {
+                requestPermission({
+                    this@SplashActivity.getCurrentLatXLngY()?.let { latXLngY ->
+                        Timber.d("location x = ${latXLngY.x} y = ${latXLngY.y} lat = ${latXLngY.lat} lon = ${latXLngY.lng}")
+                        routeNextActivity()
+                    } ?: run {
+                        Timber.d("location Error")
+                        routeNextActivity()
+                    }
+                },{
+                    Timber.d("denied")
+                }, *permissions)
+            }else{
+                Timber.d("not null $it")
+                routeNextActivity()
+            }
+        }
     }
 
     private fun routeNextActivity() {
